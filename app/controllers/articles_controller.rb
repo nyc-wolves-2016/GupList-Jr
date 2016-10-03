9fc0c9040a276d5a718e83f24a4a62cccbeaf164
Neel Gupta <neelgupta2@gmail.com>
2016-10-03 15:36:00 -0400

    Add views connected to controllers. NEXT: User auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,15 @@
 class ArticlesController < ApplicationController
+
   def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+private
+  def article_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
