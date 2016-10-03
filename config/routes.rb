8749175bf9458aad7f1b74559a712bea3473aeab
Neel Gupta <neelgupta2@gmail.com>
2016-10-03 14:48:08 -0400

    Add RESTful routes (resources) to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/show'
+  resources :articles
 
-  get 'users/new'
+  resources :users
 
-  get 'categories/index'
+  resources :categories
 
-  get 'user/index'
-
-  get 'user/show'
-
-  get 'user/new'
-
-  get 'articles/index'
-
-
-  root 'category#index'
+  root 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
