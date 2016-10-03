f8472f1ae491ef0a21dfd977b80b53e6e185394e
Neel Gupta <neelgupta2@gmail.com>
2016-10-03 16:53:59 -0400

    Working on sessions controller.

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :categories
 
   root 'categories#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login' => 'login#new'
+  post '/login/new' => 'login#create'
 end
